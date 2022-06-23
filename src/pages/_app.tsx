238234194545b99219eb1e6e39eb56c549377a89
Umut Toker <utoker@gmail.com>
2022-06-23 09:18:51 -0400

    ssr false

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,24 +14,24 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   );
 };
 
-// function getBaseUrl() {
-// if (process.browser) return ''; // Browser should use current path
-// if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-// return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-// }
+function getBaseUrl() {
+  if (process.browser) return ''; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    // const url = `${getBaseUrl()}/api/trpc`;
+    const url = `${getBaseUrl()}/api/trpc`;
     // const url = process.env.VERCEL_URL
     //   ? `https://${process.env.VERCEL_URL}/api/trpc`
     //   : 'http://localhost:3000/api/trpc';
 
-    const url = 'https://www.reqq.cc/api/trpc';
+    // const url = 'https://www.reqq.cc/api/trpc';
 
     return {
       url,
     };
   },
-  ssr: true,
+  ssr: false,
 })(MyApp);
