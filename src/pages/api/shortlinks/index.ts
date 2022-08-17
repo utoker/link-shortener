0712370094a496d913a04a855cb4587035d98979
Umut Toker <utoker@gmail.com>
2022-08-17 17:13:01 -0400

    fix short link with no session

--- src/pages/api/shortlinks/index.ts
+++ src/pages/api/shortlinks/index.ts
@@ -5,20 +5,35 @@ import { unstable_getServerSession } from 'next-auth/next';
 
 const index = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await unstable_getServerSession(req, res, authOptions);
-  const email: any = session?.user?.email;
   if (req.method !== 'POST') {
     res.status(405).json({ message: 'Method not allowed' });
   }
-  const parseBody = JSON.parse(req.body);
-  try {
-    const user = await prisma.user.findUnique({
-      where: { email },
-    });
-    const shortLink = { ...parseBody, userId: user?.id };
-    const savedShortLink = await prisma.shortlinks.create({ data: shortLink });
-    res.status(200).json(savedShortLink);
-  } catch (err) {
-    res.status(400).json({ message: 'Something went wrong' });
+  if (session === null) {
+    try {
+      const savedShortLink = await prisma.shortlinks.create({
+        data: JSON.parse(req.body),
+      });
+      res.status(200).json(savedShortLink);
+    } catch (err) {
+      res.status(400).json({ message: 'Something went wrong with no session' });
+    }
+    return;
+  }
+  if (session) {
+    const email: any = session?.user?.email;
+    const parseBody = JSON.parse(req.body);
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+      const shortLink = { ...parseBody, userId: user?.id };
+      const savedShortLink = await prisma.shortlinks.create({
+        data: shortLink,
+      });
+      res.status(200).json(savedShortLink);
+    } catch (err) {
+      res.status(400).json({ message: 'Something went wrong' });
+    }
   }
 };
 export default index;
