8572a2ef095994ca3a66c32eee47844eaacfa1ec
Umut Toker <utoker@gmail.comgit config --global user.email utoker@gmail.comgit config --global user.email utoker@gmail.com>
2025-01-02 21:56:23 -0500

    refactor: update import paths to use absolute imports for components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // /app/page.tsx
 // Home page component
 
-import ConfettiEffect from 'components/ConfettiEffect';
+import ConfettiEffect from '@components/ConfettiEffect';
 import CreateShortlinkForm from '../components/CreateShortlinkForm';
 import ShortlinksList from '../components/ShortlinksList';
-import Header from '../components/Header';
+import Header from '@components/Header';
 
 export default async function Home() {
   return (
